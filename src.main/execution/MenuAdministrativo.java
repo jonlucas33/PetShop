8c0bfc4d1b59ab024c2c168b9d0a0dceabee23c3
Lucas <joaolucaslopes296@gmail.com>
2022-06-05 21:36:34 -0300

    Adicionado menuAdministrativo().

--- src.main/execution/MenuAdministrativo.java
+++ src.main/execution/MenuAdministrativo.java
@@ -1,5 +1,45 @@
 package execution;
 
+import java.util.Scanner;
+
 public class MenuAdministrativo {
-    
+
+    static Scanner s = new Scanner(System.in);
+    int escolha;
+
+    public void menuAdministrativo() {
+        do {
+            System.out.println("BEM VINDO AO MENU ADMINISTRATIVO !!!");
+            System.out.println();
+            System.out.println("Digite 0 - Encerrar o programa");
+            System.out.println("Digite 1 - Total do valor dos atendimentos.");
+            System.out.println("Digite 2 - Quantos cachorros marcaram atendimentos.");
+            System.out.println("Digite 3 - Quantos gatos marcaram atendimentos.");
+            System.out.println("Digite 4 - Quantos banhos e tosas.");
+            switch (escolha) {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+
+                case 4:
+
+                    break;
+
+                case 0:
+                    System.out.println("Programa encerrado. Volte sempre.");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida. Digite um dos números do menu.");
+                    break;
+            }
+        } while (escolha != 0);
+    }
+
 }
--- src.main/execution/MenuGeral.java
+++ src.main/execution/MenuGeral.java
@@ -15,17 +15,16 @@ import interfaces.CalculoAtendimento;
 import sistemaDeCadastros.Cadastro;
 import sistemaDeCadastros.EfetuarCadastro;
 
-public class MenuGeral  {
+public class MenuGeral {
 
 	static Scanner s = new Scanner(System.in);
-	//ExibirFicha exibirFicha = new ExibirFicha();
 	int escolha, idade;
 	double peso;
 	String nome;
 	Cadastro cadastro;
+	MenuAdministrativo menuAdministrativo;
 
 	public void menuAnimal() throws Exception {
-		int escolha;
 		EfetuarCadastro efetuarCadastro = EfetuarCadastro.getInstance();
 		do {
 			System.out.println("BEM VINDO AO NOSSO PET SHOP !!!");
@@ -34,74 +33,78 @@ public class MenuGeral  {
 			System.out.println("Digite 1 - Criar Cadastro");
 			System.out.println("Digite 2 - Menu Cachorro");
 			System.out.println("Digite 3 - Menu Gato");
-						//Digite 4 - menu adminstrativo
-			//Mostrar balanço diário, total de dinheiro, quantos cachorros e gatos foram atendidos, quantidade de banhos e quantidade de tosas.
-			escolha=s.nextInt();
-			switch(escolha) {
-			case 1:
-				try{
-					cadastro=efetuarCadastro.realizarCadastro();
-				} catch(CadastroNaoPermitido cadastroNaoPermitido) {
-					System.out.println(cadastroNaoPermitido.getMessage());
-				} catch(CadastroNaoExiste cadastroNaoExiste) {
-					System.out.println(cadastroNaoExiste.getMessage());
-				}
-				break;
-			case 2:
-				getMenu(new Cachorro(nome, peso, idade), new CalculoAtendimentoCachorro(), cadastro);
-				break;
+			System.out.println("Digite 4 - Menu administrativo");
+			escolha = s.nextInt();
+			switch (escolha) {
+				case 1:
+					try {
+						cadastro = efetuarCadastro.realizarCadastro();
+					} catch (CadastroNaoPermitido cadastroNaoPermitido) {
+						System.out.println(cadastroNaoPermitido.getMessage());
+					} catch (CadastroNaoExiste cadastroNaoExiste) {
+						System.out.println(cadastroNaoExiste.getMessage());
+					}
+					break;
+				case 2:
+					getMenu(new Cachorro(nome, peso, idade), new CalculoAtendimentoCachorro(), cadastro);
+					break;
 
-			case 3:
-				getMenu(new Gato(nome,peso,idade), new CalculoAtendimentoGato(), cadastro);
-				break;
+				case 3:
+					getMenu(new Gato(nome, peso, idade), new CalculoAtendimentoGato(), cadastro);
+					break;
 
-			case 0:
-				System.out.println("Programa encerrado. Volte sempre.");
-				break;
+				case 4:
+					menuAdministrativo.menuAdministrativo();
+					break;
 
-			default:
-				System.out.println("Op��o inv�lida. Digite um dos n�meros do menu.");
-				break;
+				case 0:
+					System.out.println("Programa encerrado. Volte sempre.");
+					break;
+
+				default:
+					System.out.println("Opção inválida. Digite um dos números do menu.");
+					break;
 			}
 
-		}while(escolha!=0);
+		} while (escolha != 0);
 	}
 
-	public void getMenu(Animal animal,CalculoAtendimento calculoAtendimento,Cadastro c) throws TamanhoNaoPermitido, CadastroNaoExiste {
+	public void getMenu(Animal animal, CalculoAtendimento calculoAtendimento, Cadastro c)
+			throws TamanhoNaoPermitido, CadastroNaoExiste {
 		do {
-			Custos custos = new Custos(); 
+			Custos custos = new Custos();
 			System.out.println("VAMOS CUIDAR DO SEU PET !!!");
 			System.out.println();
 			System.out.println("Digite 0 - Sair.");
 			System.out.println("Digite 1 - Marcar o banho.");
 			System.out.println("Digite 2 - Marcar a Tosa.");
-			escolha=s.nextInt();
-			switch(escolha) {
-			case 1: 
-				try{
-					custos.calcularBanho(animal, calculoAtendimento,c );
-				}catch(TamanhoNaoPermitido tamanhoNaoPermitido) {
-					System.out.println(tamanhoNaoPermitido.getMessage());
-				} catch(CadastroNaoExiste cadastroNaoExiste) {
-					System.out.println(cadastroNaoExiste.getMessage());
-				}
-				break;
-			case 2: 
-				try{
-					custos.calcularTosa(animal, calculoAtendimento,c);
-				}catch(TamanhoNaoPermitido tamanhoNaoPermitido) {
-					System.out.println(tamanhoNaoPermitido.getMessage());
-				} catch(CadastroNaoExiste cadastroNaoExiste) {
-					System.out.println(cadastroNaoExiste.getMessage());
-				}
-				break;
-			case 0:
-				System.out.println("Programa encerrado.");
-				break;
-			default:
-				System.out.println("Op��o inv�lida. Digite um dos n�meros do menu.");
-				break;
+			escolha = s.nextInt();
+			switch (escolha) {
+				case 1:
+					try {
+						custos.calcularBanho(animal, calculoAtendimento, c);
+					} catch (TamanhoNaoPermitido tamanhoNaoPermitido) {
+						System.out.println(tamanhoNaoPermitido.getMessage());
+					} catch (CadastroNaoExiste cadastroNaoExiste) {
+						System.out.println(cadastroNaoExiste.getMessage());
+					}
+					break;
+				case 2:
+					try {
+						custos.calcularTosa(animal, calculoAtendimento, c);
+					} catch (TamanhoNaoPermitido tamanhoNaoPermitido) {
+						System.out.println(tamanhoNaoPermitido.getMessage());
+					} catch (CadastroNaoExiste cadastroNaoExiste) {
+						System.out.println(cadastroNaoExiste.getMessage());
+					}
+					break;
+				case 0:
+					System.out.println("Programa encerrado.");
+					break;
+				default:
+					System.out.println("Opção inválida. Digite um dos números do menu.");
+					break;
 			}
-		}while(escolha!=0);
+		} while (escolha != 0);
 	}
 }
